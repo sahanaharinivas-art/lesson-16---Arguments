c977e0b16f9a924d68c8e24640264c3b601d92f4
harinivassahana-max <harinivassahana@gmail.com>
2025-12-02 16:43:02 +1100

    buffer 1

---
+++ buffer activity 2 .py
@@ -0,0 +1,10 @@
+def shutdown (s):
+    if s -- "yes":
+        return "shutting down..."
+    elif s --"no":
+        return "shutdown aborted."
+    else:
+        return "sorry, i didnt understand that."
+
+user_input = input("do you want to shut down? (yes/no):")
+print(shutdown(user_input))
---
+++ buffer activity.py
@@ -0,0 +1,9 @@
+def employee(name):
+    print(name)
+
+def salary(exp):
+    if exp>=3:
+        return 1000000
+    else:
+        return 500000
+employee("ram")
