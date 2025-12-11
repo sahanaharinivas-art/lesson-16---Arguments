a4153a872b1124ac87bc155ee00ed4efb1571614
harinivassahana-max <harinivassahana@gmail.com>
2025-12-11 16:24:20 +1100

    assignment

--- buffer activity 2 .py
+++ buffer activity 2 .py
@@ -1,10 +1,10 @@
 def shutdown (s):
-    if s -- "yes":
+    if s - "yes":
         return "shutting down..."
-    elif s --"no":
+    elif s -"no":
         return "shutdown aborted."
     else:
-        return "sorry, i didnt understand that."
+        return "sorry."
 
 user_input = input("do you want to shut down? (yes/no):")
 print(shutdown(user_input))
